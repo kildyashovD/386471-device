639afba20482877895613e6bbe86b4bd886f102d
Kildyashov <denis.kildiaschov@yandex.ru>
2018-04-02 19:54:33 +0300

    Правки к ревью

--- js/main.js
+++ js/main.js
@@ -2,40 +2,42 @@
 
 function writeUsShow(evt) {
   evt.preventDefault();
-  document.getElementById("write-us-modal").style.display = "block";
+  writeUsModal.style.display = "block";
   modalOverlay.style.display = "block";
+  document.getElementById("modal-user-name").focus();
 }
 
 function writeUsClose(evt) {
   evt.preventDefault();
-  document.getElementById("write-us-modal").style.display = "none";
+  writeUsModal.style.display = "none";
   modalOverlay.style.display = "none";
 }
 
+var writeUsModal = document.getElementById("write-us-modal");
 var modalOverlay = document.querySelector(".modal-overlay");
-
-var writeUsModal = document.getElementById("write-us-modal-show");
-writeUsModal.addEventListener("click", writeUsShow, true);
-
+var writeUsModalBtn = document.getElementById("write-us-modal-show");
 var writeUsCloseBtn = document.getElementById("write-us-close");
+
+writeUsModalBtn.addEventListener("click", writeUsShow, true);
 writeUsCloseBtn.addEventListener("click", writeUsClose, true);
 
 // YANDEX MAP MODAL
 
 function yandexMapShow(evt) {
   evt.preventDefault();
-  document.getElementById("maps-modal").style.display = "block";
+  yandexMapModal.style.display = "block";
   modalOverlay.style.display = "block";
 }
 
 function yandexMapClose(evt) {
   evt.preventDefault();
-  document.getElementById("maps-modal").style.display = "none";
+  yandexMapModal.style.display = "none";
   modalOverlay.style.display = "none";
 }
 
-var yandexMapModalBtn = document.getElementById("maps-modal-link");
-yandexMapModalBtn.addEventListener("click", yandexMapShow, true);
-
+var yandexMapModal = document.getElementById("maps-modal");
+var yandexMapModalShowBtn = document.getElementById("maps-modal-link");
 var yandexMapModalCloseBtn = document.getElementById("maps-modal-close");
+
+yandexMapModalShowBtn.addEventListener("click", yandexMapShow, true);
 yandexMapModalCloseBtn.addEventListener("click", yandexMapClose, true);
